e02f68cc84122561f84973918ce0792593d8c1d4
ricopenkalski <51368644+ricopenkalski@users.noreply.github.com>
2024-09-29 20:25:56 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
+const observerright = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('überuns__visible');
+        } else {
+            entry.target.classList.remove('überuns__visible');
+        }
+    });
+});
+
+const observerleft = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('überuns__visible');
+        } else {
+            entry.target.classList.remove('überuns__visible');
+        }
+    });
+});
+
+const hiddenElementsright = document.querySelectorAll('.überuns__hidden.right');
+hiddenElementsright.forEach((element) => observerright.observe(element));
+
+const hiddenElementsleft = document.querySelectorAll('.überuns__hidden.left');
+hiddenElementsleft.forEach((element) => observerleft.observe(element));
